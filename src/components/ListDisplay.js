573346e12dc3357f1fb8129345ef0d0325c68889
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-12-19 15:33:47 -0500

    Added Icons for edit and delete

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -1,21 +1,36 @@
 import React from "react";
-import "./ListDisplay.css"
-import {useState} from "react";
+import "./ListDisplay.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash} from '@fortawesome/free-solid-svg-icons'
+import { faEdit} from '@fortawesome/free-solid-svg-icons'
+// import {useState} from "react";
 
 const ListDisplay = (props) => {
-    // console.log("props",props)
+
+    const trashCan = <FontAwesomeIcon icon={faTrash}/>
+    const edit = <FontAwesomeIcon icon={faEdit}/>
+    console.log("props",props)
     return(
         <div className = "wrapper"> 
-            <div className="item">
-                {props.listOfItems.map((groceryItem) => {
-                                
-                                return <p>{groceryItem}</p>
-                            })}
+            <div className="items">
+                {props.listOfItems.map((groceryItem,index) => {
+                                console.log(index)
+                                return( 
+                                <div className="itemHolder">
+                                    <span className="editIcon">{edit}</span>
+                                     <p className="item" key={index}>{groceryItem}</p>
+                                </div>
+                );})}
             </div>
-            <div className="price">
-                {props.price.map((priceItem) => {
+            <div className="prices">
+                {props.price.map((priceItem,index) => {
                     
-                    return <p>${priceItem}</p>
+                    return (<div className="priceHolder">
+
+                                <p className="price" key={index}>${priceItem}</p>
+                                <span className="trashIcon">{trashCan}</span>
+
+                             </div>)
                 })}
             </div>
         </div>
